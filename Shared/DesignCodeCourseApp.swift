7f02ca519cbac6579f1b032febe45d3b83df8c9f
Chorn Thorn <chornthorn.kh@gmail.com>
2021-11-16 19:13:01 +0700

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by One Sala Dev on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
